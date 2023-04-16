b572def52496cf79be0d6563473a997681ae909d
FrancoisSuzeau <frsuzeau@gmail.com>
2023-04-16 15:08:46 +0200

    Correct a little bug with the asteroid count slider size

--- Solar-System-2.0/Engine/GUI/Menu.cpp
+++ Solar-System-2.0/Engine/GUI/Menu.cpp
@@ -226,6 +226,8 @@ void Menu::render(int width, int height)
     ImVec2 frame_padding_save = style.FramePadding;
 
     window_flags |= ImGuiWindowFlags_NoResize;
+    window_flags |= ImGuiWindowFlags_AlwaysAutoResize;
+    window_flags |= ImGuiWindowFlags_NoScrollbar;
     ImGui::SetNextWindowPos(ImVec2(((float)width / 2.f) - 200.f, ((float)height / 2.f) - 200.f));
     ImGui::SetNextWindowSize(ImVec2(400, 400));
 
